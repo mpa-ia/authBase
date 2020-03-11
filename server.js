1d8f7692bc8b613653691920439fccd9c3ceee62
Marysia Pabich <marysia.pabich@gmail.com>
2020-03-11 22:53:48 +0100

    Separate passport config from server file

--- server.js
+++ server.js
@@ -3,28 +3,10 @@ const cors = require('cors');
 const path = require('path');
 const hbs = require('express-handlebars');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const session = require('express-session');
+const passportConfig = require('./config/passport');
 
 const app = express();
-
-passport.use(new GoogleStrategy({
-  clientID: '',
-  clientSecret: '',
-  callbackURL: 'http://localhost:8000/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-  done(null, profile);
-}));
-
-// serialize user when saving to session
-passport.serializeUser((user, serialize) => {
-  serialize(null, user);
-});
-
-// deserialize user when reading from session
-passport.deserializeUser((obj, deserialize) => {
-  deserialize(null, obj);
-});
 
 app.use(session({ secret: 'authbase project session string' }));
 app.use(passport.initialize());
