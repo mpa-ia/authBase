08732420737b50bcb27b4f33cc5830f45feb6752
Marysia Pabich <marysia.pabich@gmail.com>
2020-03-11 23:03:02 +0100

    Move auth & user endpoints to separate files

--- server.js
+++ server.js
@@ -24,22 +24,8 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/auth/google',
-  passport.authenticate('google', { scope: ['email', 'profile'] }));
-
-app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/user/no-permission' }),
-  (req, res) => {
-    res.redirect('/user/logged');
-  }
-);
-
-app.get('/user/logged', (req, res) => {
-  res.render('logged');
-});
-
-app.get('/user/no-permission', (req, res) => {
-  res.render('noPermission');
-});
+app.use('/auth', require('./routes/auth.routes'));
+app.use('/user', require('./routes/user.routes'));
 
 app.use('/', (req, res) => {
   res.status(404).render('notFound');
