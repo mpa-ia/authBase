809d614f19f8d02933838b1f186494549f8aab99
Marysia Pabich <marysia.pabich@gmail.com>
2020-03-14 14:59:53 +0100

    Restrict access to /user pages only for authenticated users

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,12 +2,20 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/logged', (req, res) => {
-  res.render('logged');
+  !req.user ? res.redirect('/no-permission') : res.render('logged');
+});
+
+router.get('/profile', (req, res) => {
+  !req.user ? res.redirect('/no-permission') : res.render('userProfile');
+});
+
+router.get('/profile/settings', (req, res) => {
+  console.log(req.user);
+  !req.user ? res.redirect('/no-permission') : res.render('userProfileSettings');
 });
 
 router.get('/no-permission', (req, res) => {
   res.render('noPermission');
 });
-
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const session = require('express-session');
 const passportConfig = require('./config/passport');
 
 const app = express();
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(session({ secret: 'authbase project session string' }));
 app.use(passport.initialize());
@@ -18,7 +19,7 @@ app.set('view engine', '.hbs');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '/public')));
+
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -26,6 +27,12 @@ app.get('/', (req, res) => {
 
 app.use('/auth', require('./routes/auth.routes'));
 app.use('/user', require('./routes/user.routes'));
+
+app.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    res.redirect('/');
+  });
+});
 
 app.use('/', (req, res) => {
   res.status(404).render('notFound');
